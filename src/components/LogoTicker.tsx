e9998826727466a783885cbd82a9431ddb92c73e
mohdajnaas <mohmedajnaskp@gmail.com>
2026-07-14 18:57:10 +0530

    responsive

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -13,17 +13,33 @@ export default function LogoTicker() {
         </div>
         
         <div className={styles.right}>
-          <div className={styles.logo}>
-            <span style={{fontWeight: 900, fontSize: '20px', letterSpacing: '-1px'}}>RedBull</span>
-          </div>
-          <div className={styles.logo}>
-            <span style={{fontWeight: 900, fontSize: '24px', letterSpacing: '-2px'}}>JD</span>
-          </div>
-          <div className={styles.logo}>
-            <span style={{fontWeight: 700, fontSize: '18px', letterSpacing: '1px'}}>XBOX</span>
-          </div>
-          <div className={styles.logo}>
-            <span style={{fontWeight: 800, fontSize: '20px', color: '#ff7a59'}}>HubSpot</span>
+          <div className={styles.marqueeContent}>
+            {/* Set 1 */}
+            <div className={styles.logo}>
+              <span style={{fontWeight: 900, fontSize: '20px', letterSpacing: '-1px'}}>RedBull</span>
+            </div>
+            <div className={styles.logo}>
+              <span style={{fontWeight: 900, fontSize: '24px', letterSpacing: '-2px'}}>JD</span>
+            </div>
+            <div className={styles.logo}>
+              <span style={{fontWeight: 700, fontSize: '18px', letterSpacing: '1px'}}>XBOX</span>
+            </div>
+            <div className={styles.logo}>
+              <span style={{fontWeight: 800, fontSize: '20px', color: '#ff7a59'}}>HubSpot</span>
+            </div>
+            {/* Set 2 (Duplicated for seamless loop) */}
+            <div className={styles.logo}>
+              <span style={{fontWeight: 900, fontSize: '20px', letterSpacing: '-1px'}}>RedBull</span>
+            </div>
+            <div className={styles.logo}>
+              <span style={{fontWeight: 900, fontSize: '24px', letterSpacing: '-2px'}}>JD</span>
+            </div>
+            <div className={styles.logo}>
+              <span style={{fontWeight: 700, fontSize: '18px', letterSpacing: '1px'}}>XBOX</span>
+            </div>
+            <div className={styles.logo}>
+              <span style={{fontWeight: 800, fontSize: '20px', color: '#ff7a59'}}>HubSpot</span>
+            </div>
           </div>
         </div>
       </div>
